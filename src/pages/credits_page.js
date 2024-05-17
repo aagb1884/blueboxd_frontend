caf3bad595660b72999d001c20b105d84ce4bd12
andrew <113289014+aagb1884@users.noreply.github.com>
2024-05-17 15:17:46 +0100

    centered loading gif

--- src/pages/credits_page.js
+++ src/pages/credits_page.js
@@ -6,9 +6,10 @@ const Credits = () => {
         <PageLayout>
         <section>
             <h2>Credits</h2>
+            <h4>A list of image and data sources used in making this website.</h4>
             <p>Background Starfield Image from <a href="https://www.flickr.com/photos/galet09/15447326299">galet09 on Flickr</a> used unedited under a <a href="https://creativecommons.org/licenses/by/2.0/">Creative Commons BY 2.0 Deed.</a></p>
             <p>Animated Clock from <a href="https://commons.wikimedia.org/wiki/File:SBB_railway_clock_animated.gif">on Wikimedia Commons</a> used unedited under a <a href="https://creativecommons.org/licenses/by-sa/4.0/deed.en">Creative Commons BY-SA 4.0 Deed.</a></p>
-            <p>Default Avatarfrom <a href="https://commons.wikimedia.org/wiki/File:User_%2889041%29_-_The_Noun_Project.svg">on Wikimedia Commons</a> used unedited under a <a href="https://creativecommons.org/publicdomain/zero/1.0/deed.en">Creative Commons 1.0 Universal Deed.</a></p>
+            <p>Default Avatar from <a href="https://commons.wikimedia.org/wiki/File:User_%2889041%29_-_The_Noun_Project.svg">on Wikimedia Commons</a> used unedited under a <a href="https://creativecommons.org/publicdomain/zero/1.0/deed.en">Creative Commons 1.0 Universal Deed.</a></p>
         </section>
         </PageLayout>
      );
